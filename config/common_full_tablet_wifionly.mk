c61839fb1c04eb1f3a7c8319cf00b9ecc77c580b
Arseniy Graur <argraur12@yandex.ru>
2016-10-15 19:16:05 +0300

    # Bootanimation

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit common Bliss stuff
 $(call inherit-product, vendor/bliss/config/common_full.mk)
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/bliss/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif
+##ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+##    PRODUCT_COPY_FILES += \
+##        vendor/bliss/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
+##endif
