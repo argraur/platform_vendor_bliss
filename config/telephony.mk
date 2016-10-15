b4b53cc9ef2a2a74e98fe83173479f9d1771df80
Arseniy Graur <argraur12@yandex.ru>
2016-10-15 20:00:35 +0300

    Create telephony.mk

---
+++ config/telephony.mk
@@ -0,0 +1,9 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/bliss/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    messaging \
+    Stk \
+    CellBroadcastReceiver
