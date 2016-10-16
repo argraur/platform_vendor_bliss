8016ee12343f4ea36b22c7a81ee356943c054131
Arseniy Graur <argraur12@yandex.ru>
2016-10-16 10:37:39 +0300

    Add lt01 combos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 for combo in $(cat vendor/bliss/bliss-device-targets)
+for lt01combo in $(cat vendor/bliss/custom/bliss-lt01-targets)
 do
     add_lunch_combo $combo
+    add_lunch_combo $lt01combo
 done
